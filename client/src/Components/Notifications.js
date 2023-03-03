47b87c13c2839664588e2093b09e166c10a4464c
t3j3ndrA <tejendradhanani@gmail.com>
2023-03-04 01:30:26 +0530

    Mark notification read

--- client/src/Components/Notifications.js
+++ client/src/Components/Notifications.js
@@ -10,24 +10,40 @@ const Notifications = () => {
 	const [notifications, setNotifications] = useState([]);
 	const [key, setKey] = useState(Math.random());
 	const [isConnected, setIsConnected] = useState(socket.connected);
+	const [selectedNotifications, setSelectedNotifications] = useState(new Set());
+
+	console.log(selectedNotifications);
 
 	const fetchUsersNotifications = async () => {
-		console.log("Fetching for >>", userId);
 		axios
 			.get(`http://localhost:5000/api/noti/of/${userId}`)
 			.then(({ data }) => {
-				console.log("resp >>", data);
 				setNotifications(data.data);
 				setKey(Math.random());
 			})
 			.catch((err) => console.log(err));
 	};
 
+	const handleMarkRead = (e) => {
+		e.preventDefault();
+
+		const data = { notifications: Array.from(selectedNotifications) };
+		axios
+			.put(`http://localhost:5000/api/noti/mark-read/${userId}`, data)
+			.then(({ data }) => {
+				setSelectedNotifications(new Set());
+				fetchUsersNotifications();
+				setKey(Math.random());
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
 	useEffect(() => {
 		fetchUsersNotifications();
 
 		socket.on("connect", () => {
-			console.log("connected");
 			setIsConnected(true);
 		});
 
@@ -49,21 +65,52 @@ const Notifications = () => {
 		};
 	}, []);
 
-	console.log(notifications);
-
 	return (
 		<>
 			<div className="p-4 w-full h-screen">
 				<h1 className="text-2xl font-medium">Notifications</h1>
 				<div className="h-1 mt-2 mb-4 rounded-full bg-black"></div>
 				<div className="flex flex-col  overflow-y-scroll h-[89vh]" key={key}>
-					{notifications?.map(({ msg, forUsers }, idx) => {
+					<div className="flex flex-row gap-2 justify-start items-center mb-4">
+						<div>
+							<input
+								type={"checkbox"}
+								checked={notifications?.length === selectedNotifications.size}
+								onClick={() => {
+									let s = new Set();
+									if (selectedNotifications.size == notifications?.length) {
+										setSelectedNotifications(s);
+									} else {
+										notifications.forEach((item) => {
+											s.add(item._id);
+										});
+										setSelectedNotifications(s);
+									}
+									setKey(Math.random());
+								}}
+							/>
+							<span className="ml-1">All</span>
+						</div>
+						<button
+							type="button"
+							onClick={handleMarkRead}
+							className="bg-pink-500 text-white rounded-md px-2 py-[1px]"
+						>
+							Mark As Read
+						</button>
+					</div>
+
+					{notifications?.map(({ msg, forUsers, _id }, idx) => {
 						return (
 							<>
 								<NotificationCard
+									_id={_id}
 									msg={msg}
 									read={forUsers[0].read}
 									createdAt={forUsers[0]?.createdAt}
+									selectedNotifications={selectedNotifications}
+									setSelectedNotifications={setSelectedNotifications}
+									setKey={setKey}
 								/>
 							</>
 						);
@@ -74,12 +121,39 @@ const Notifications = () => {
 	);
 };
 
-const NotificationCard = ({ msg, read, createdAt }) => {
+const NotificationCard = ({
+	_id,
+	msg,
+	read,
+	createdAt,
+	selectedNotifications,
+	setSelectedNotifications,
+	setKey,
+}) => {
 	const dt = new Date(createdAt);
 
 	return (
 		<>
-			<div className="border-b flex space-x-4  p-2">
+			<div
+				className={`border-b  flex space-x-4  p-2 ${
+					read ? "" : "bg-slate-200"
+				}`}
+			>
+				<input
+					type={"checkbox"}
+					checked={selectedNotifications.has(_id)}
+					onClick={() => {
+						let s = selectedNotifications;
+						if (s.has(_id)) {
+							s.delete(_id);
+						} else {
+							s.add(_id);
+						}
+						// setCheckKey(Math.random());
+						setSelectedNotifications(s);
+						setKey(Math.random());
+					}}
+				/>
 				<img src="../images/avatar.jpg" className="w-12 h-12 rounded-full " />
 				<div>
 					{/* <h4 className="font-medium">{read ? "read" : "unread"}</h4> */}
