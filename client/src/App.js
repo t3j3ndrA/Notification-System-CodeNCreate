5a9502be939b3f116c89ad82613a6a9887083d03
my1243 <mihiryarra@gmail.com>
2023-03-03 19:11:11 +0530

    pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,39 @@
-import { useEffect, useState } from "react";
+import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Outlet,
+  } from "react-router-dom";
+import SideNavbar from './Components/Navbar';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import ChattingPage from './Components/Chat';
+
 
 function App() {
-	useEffect(() => {
-		return () => {};
-	}, []);
-
-	return <div className="text-red-500">App.js</div>;
+  return (
+    <>
+        <>
+            <Router>
+                <Routes>
+                    <Route element={
+                        <>
+                            <div className='flex'>
+                            <SideNavbar/>
+                            <Outlet/>
+                            </div>
+                        </>
+                    }>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/chat' element={<ChattingPage/>}/>
+               </Route>
+               <Route path='/login' element={<Login/>}/>
+                </Routes>
+            </Router>
+        </>
+    </>
+  );
 }
 
 export default App;
