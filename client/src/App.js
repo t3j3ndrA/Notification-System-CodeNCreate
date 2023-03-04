5c01826735a196f7ddb959c3f7aca48bab73e7c9
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2023-03-04 07:18:51 +0530

    removed unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,42 @@
-import './App.css';
+import "./App.css";
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Outlet,
-  } from "react-router-dom";
-import SideNavbar from './Components/Navbar';
-import Home from './Components/Home';
-import Login from './Components/Login';
-import ChattingPage from './Components/Chat';
-import Notifications from './Components/Notifications';
-
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import SideNavbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import ChattingPage from "./Components/Chat";
+import Notifications from "./Components/Notifications";
 
 function App() {
   return (
     <>
-        <>
-        <div className='bg-white dark:bg-slate-800'>
-            <Router>
-                <Routes>
-                    <Route element={
-                        <>
-                            <div className='flex'>
-                            <SideNavbar/>
-                            <Outlet/>
-                            </div>
-                        </>
-                    }>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/messages' element={<ChattingPage/>}/>
-                    <Route path='/notifications' element={<Notifications/>}/>
-               </Route>
-               <Route path='/login' element={<Login/>}/>
-                </Routes>
-            </Router>
-            </div>
-        </>
+      <>
+        <div className="bg-white dark:bg-slate-800">
+          <Router>
+            <Routes>
+              <Route
+                element={
+                  <>
+                    <div className="flex">
+                      <SideNavbar />
+                      <Outlet />
+                    </div>
+                  </>
+                }
+              >
+                <Route path="/" element={<Home />} />
+                <Route path="/messages" element={<ChattingPage />} />
+                <Route path="/notifications" element={<Notifications />} />
+              </Route>
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </Router>
+        </div>
+      </>
     </>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from './ThemeContext';
-import Background from './Background';
-import Toggle from './ThemeToggle';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "./ThemeContext";
+import Background from "./Background";
+import Toggle from "./ThemeToggle";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <ThemeProvider>
-    <Background>
-    <div className="fixed z-50 right-0 top-0 mt-2 mr-2">
-          <Toggle/>
-      </div>
-    <App />
-    </Background>
-  </ThemeProvider>
+    <ThemeProvider>
+      <Background>
+        <div className="fixed z-50 right-0 top-0 mt-2 mr-2">
+          <Toggle />
+        </div>
+        <App />
+      </Background>
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
