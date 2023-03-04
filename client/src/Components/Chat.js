1faafa3b965b8b6d34d636b94136282a93d27763
t3j3ndrA <tejendradhanani@gmail.com>
2023-03-04 12:02:00 +0530

    Toastiy integration with navigation to respective page

--- client/src/Components/Chat.js
+++ client/src/Components/Chat.js
@@ -1,145 +1,156 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FiSend } from "react-icons/fi";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 
 import { io } from "socket.io-client";
 const socket = io("http://localhost:5000");
 
 const ChattingPage = () => {
-  const userId = localStorage.getItem("_id");
-  const username = localStorage.getItem("username");
+	const userId = localStorage.getItem("_id");
+	const username = localStorage.getItem("username");
 
-  const [chatRoom, setChatRoom] = useState({});
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [key, setKey] = useState(Math.random());
+	const [chatRoom, setChatRoom] = useState({});
+	const [isConnected, setIsConnected] = useState(socket.connected);
+	const [key, setKey] = useState(Math.random());
+	const navigate = useNavigate();
+	const chatRef = useRef(null);
 
-  const fetchChatRoom = async () => {
-    axios
-      .get(`http://localhost:5000/api/room/6401fb9bfc859f42a023419c`)
-      .then(({ data }) => {
-        setChatRoom(data.data);
-        setKey(Math.random());
-      })
-      .catch((err) => console.log(err));
-  };
+	const fetchChatRoom = async () => {
+		axios
+			.get(`http://localhost:5000/api/room/6401fb9bfc859f42a023419c`)
+			.then(({ data }) => {
+				setChatRoom(data.data);
+				setKey(Math.random());
+			})
+			.catch((err) => console.log(err));
+	};
 
-  const sendMessage = async () => {
-    axios
-      .put(`http://localhost:5000/api/room/6401fb9bfc859f42a023419c`, {
-        username,
-        sender: userId,
-        msg: newMessage,
-      })
-      .then(({ data }) => {
-        setChatRoom(data.data);
-        setKey(Math.random());
-        setNewMessage("");
-      })
-      .catch((err) => console.log(err));
-  };
+	const sendMessage = async () => {
+		axios
+			.put(`http://localhost:5000/api/room/6401fb9bfc859f42a023419c`, {
+				username,
+				sender: userId,
+				msg: newMessage,
+			})
+			.then(({ data }) => {
+				setChatRoom(data.data);
+				setKey(Math.random());
+				setNewMessage("");
+			})
+			.catch((err) => console.log(err));
+	};
 
-  useEffect(() => {
-    fetchChatRoom();
+	useEffect(() => {
+		fetchChatRoom();
 
-    socket.on("connect", () => {
-      console.log("connected");
-      setIsConnected(true);
-    });
+		socket.on("connect", () => {
+			console.log("connected");
+			setIsConnected(true);
+		});
 
-    socket.on("disconnect", () => {
-      setIsConnected(false);
-    });
+		socket.on("disconnect", () => {
+			setIsConnected(false);
+		});
 
-    socket.on("new-msg", () => {
-      fetchChatRoom();
-    });
+		socket.on("new-msg", () => {
+			fetchChatRoom();
+			chatRef.current.scrollIntoView({ behavior: "smooth" });
+		});
 
-    socket.on("new-noti", (notification) => {});
+		socket.on("new-noti", ({ notificationMsg, username: u }) => {
+			console.log(notificationMsg);
+			console.log(u);
+			if (username != u) showToastMessage(notificationMsg);
+		});
 
-    return () => {
-      socket.off("connect");
-      socket.off("disconnect");
-      socket.off("new-noti");
-      socket.off("new-msg");
-    };
-  }, []);
+		return () => {
+			socket.off("connect");
+			socket.off("disconnect");
+			socket.off("new-noti");
+			socket.off("new-msg");
+		};
+	}, []);
 
-  const [newMessage, setNewMessage] = useState("");
+	const [newMessage, setNewMessage] = useState("");
 
-  const handleChange = (event) => {
-    setNewMessage(event.target.value);
-  };
+	const handleChange = (event) => {
+		setNewMessage(event.target.value);
+	};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    sendMessage();
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		sendMessage();
+	};
 
-  const showToastMessage = (msg) => {
-    toast.success(msg, {
-      position: toast.POSITION.BOTTOM_RIGHT,
-    });
-  };
-  return (
-    <>
-      <div className="flex flex-col md:h-screen h-[92vh] w-full">
-        <header className="bg-pink-500 text-white flex items-center justify-between px-4 py-3">
-          <div className="flex items-center">
-            <img
-              className="w-10 h-10 object-cover rounded-full mr-4"
-              src="https://source.unsplash.com/random/40x40"
-              alt="Group Icon"
-            />
-            <h1 className="font-bold text-xl">{chatRoom?.name}</h1>
-          </div>
-        </header>
-        <main className="flex-1 p-4 overflow-y-scroll" key={key}>
-          {chatRoom?.messages?.map(
-            ({ username, sender, msg, createdAt }, index) => {
-              const dt = new Date(createdAt);
-              return (
-                <div
-                  key={index}
-                  className={`${
-                    sender == userId ? "text-right" : "text-left"
-                  } mb-4`}
-                >
-                  <p className="font-medium mb-1">{username}</p>
-                  <p className="bg-purple-200 rounded-lg py-2 px-3 inline-block">
-                    {msg}
-                  </p>
-                  <p className="text-gray-600 text-sm">
-                    {dt.getHours() + ":" + dt.getMinutes()}
-                  </p>
-                </div>
-              );
-            }
-          )}
-        </main>
-        <footer className="bg-white px-4 py-3">
-          <form onSubmit={handleSubmit} className="flex">
-            <input
-              type="text"
-              className="flex-1 border-gray-300 border-2 rounded-lg py-2 px-3 mr-2 focus:outline-none"
-              placeholder="Type a message..."
-              value={newMessage}
-              onChange={handleChange}
-            />
-            <button
-              className="bg-blue-500 text-2xl hover:scale-105 cursor-pointer text-white font-bold py-2 px-4 rounded-lg"
-              type="submit"
-              disabled={!newMessage}
-            >
-              <FiSend className="" />
-            </button>
-          </form>
-        </footer>
-      </div>
-      <ToastContainer onClick={() => {}} />
-    </>
-  );
+	const showToastMessage = (msg) => {
+		toast.success(msg, {
+			position: toast.POSITION.BOTTOM_RIGHT,
+			onClick: () => {
+				navigate("/notifications");
+			},
+		});
+	};
+	return (
+		<>
+			<div className="flex flex-col md:h-screen h-[92vh] w-full">
+				<header className="bg-pink-500 text-white flex items-center justify-between px-4 py-3">
+					<div className="flex items-center">
+						<img
+							className="w-10 h-10 object-cover rounded-full mr-4"
+							src="https://source.unsplash.com/random/40x40"
+							alt="Group Icon"
+						/>
+						<h1 className="font-bold text-xl">{chatRoom?.name}</h1>
+					</div>
+				</header>
+				<main ref={chatRef} className="flex-1 p-4 overflow-y-scroll " key={key}>
+					{chatRoom?.messages?.map(
+						({ username, sender, msg, createdAt }, index) => {
+							const dt = new Date(createdAt);
+							return (
+								<div
+									key={index}
+									className={`${
+										sender == userId ? "text-right" : "text-left"
+									} mb-4`}
+								>
+									<p className="font-medium mb-1">{username}</p>
+									<p className="bg-purple-200 rounded-lg py-2 px-3 inline-block">
+										{msg}
+									</p>
+									<p className="text-gray-600 text-sm">
+										{dt.getHours() + ":" + dt.getMinutes()}
+									</p>
+								</div>
+							);
+						}
+					)}
+				</main>
+				<footer className="bg-white px-4 py-3">
+					<form onSubmit={handleSubmit} className="flex">
+						<input
+							type="text"
+							className="flex-1 border-gray-300 border-2 rounded-lg py-2 px-3 mr-2 focus:outline-none"
+							placeholder="Type a message..."
+							value={newMessage}
+							onChange={handleChange}
+						/>
+						<button
+							className="bg-blue-500 text-2xl hover:scale-105 cursor-pointer text-white font-bold py-2 px-4 rounded-lg"
+							type="submit"
+							disabled={!newMessage}
+						>
+							<FiSend className="" />
+						</button>
+					</form>
+				</footer>
+			</div>
+			<ToastContainer />
+		</>
+	);
 };
 
 export default ChattingPage;
--- client/src/Components/Notifications.js
+++ client/src/Components/Notifications.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -7,173 +8,181 @@ import { io } from "socket.io-client";
 const socket = io("http://localhost:5000");
 
 const Notifications = () => {
-  const userId = localStorage.getItem("_id");
-  const username = localStorage.getItem("username");
-  const [notifications, setNotifications] = useState([]);
-  const [key, setKey] = useState(Math.random());
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [selectedNotifications, setSelectedNotifications] = useState(new Set());
+	const userId = localStorage.getItem("_id");
+	const username = localStorage.getItem("username");
+	const [notifications, setNotifications] = useState([]);
+	const [key, setKey] = useState(Math.random());
+	const [isConnected, setIsConnected] = useState(socket.connected);
+	const [selectedNotifications, setSelectedNotifications] = useState(new Set());
+	const navigate = useNavigate();
 
-  console.log(selectedNotifications);
+	console.log(selectedNotifications);
 
-  const fetchUsersNotifications = async () => {
-    axios
-      .get(`http://localhost:5000/api/noti/of/${userId}`)
-      .then(({ data }) => {
-        setNotifications(data.data);
-        setKey(Math.random());
-      })
-      .catch((err) => console.log(err));
-  };
+	const fetchUsersNotifications = async () => {
+		axios
+			.get(`http://localhost:5000/api/noti/of/${userId}`)
+			.then(({ data }) => {
+				setNotifications(data.data);
+				setKey(Math.random());
+			})
+			.catch((err) => console.log(err));
+	};
 
-  const handleMarkRead = (e) => {
-    e.preventDefault();
+	const handleMarkRead = (e) => {
+		e.preventDefault();
 
-    const data = { notifications: Array.from(selectedNotifications) };
-    axios
-      .put(`http://localhost:5000/api/noti/mark-read/${userId}`, data)
-      .then(({ data }) => {
-        setSelectedNotifications(new Set());
-        fetchUsersNotifications();
-        setKey(Math.random());
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+		const data = { notifications: Array.from(selectedNotifications) };
+		axios
+			.put(`http://localhost:5000/api/noti/mark-read/${userId}`, data)
+			.then(({ data }) => {
+				setSelectedNotifications(new Set());
+				fetchUsersNotifications();
+				setKey(Math.random());
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-  useEffect(() => {
-    fetchUsersNotifications();
+	useEffect(() => {
+		fetchUsersNotifications();
 
-    socket.on("connect", () => {
-      setIsConnected(true);
-    });
+		socket.on("connect", () => {
+			setIsConnected(true);
+		});
 
-    socket.on("disconnect", () => {
-      setIsConnected(false);
-    });
+		socket.on("disconnect", () => {
+			setIsConnected(false);
+		});
 
-    socket.on("new-msg", () => {});
+		socket.on("new-msg", ({ notificationMsg, username: u }) => {
+			console.log(notificationMsg);
+			console.log(u);
+			if (username != u) showToastMessage(notificationMsg);
+		});
 
-    socket.on("new-noti", (notification) => {
-      fetchUsersNotifications();
-    });
+		socket.on("new-noti", (notification) => {
+			fetchUsersNotifications();
+		});
 
-    return () => {
-      socket.off("connect");
-      socket.off("disconnect");
-      socket.off("new-noti");
-      socket.off("new-msg");
-    };
-  }, []);
+		return () => {
+			socket.off("connect");
+			socket.off("disconnect");
+			socket.off("new-noti");
+			socket.off("new-msg");
+		};
+	}, []);
 
-  const showToastMessage = (msg) => {
-    toast.success(msg, {
-      position: toast.POSITION.BOTTOM_RIGHT,
-    });
-  };
+	const showToastMessage = (msg) => {
+		toast.success(msg, {
+			position: toast.POSITION.BOTTOM_RIGHT,
+			onClick: () => {
+				navigate("/messages");
+			},
+		});
+	};
 
-  return (
-    <>
-      <div className="p-4 w-full h-screen">
-        <h1 className="text-2xl font-medium">Notifications</h1>
-        <div className="h-1 mt-2 mb-4 rounded-full bg-black"></div>
-        <div className="flex flex-col  overflow-y-scroll h-[89vh]" key={key}>
-          <div className="flex flex-row gap-2 justify-start items-center mb-4">
-            <div>
-              <input
-                type={"checkbox"}
-                checked={notifications?.length === selectedNotifications.size}
-                onClick={() => {
-                  let s = new Set();
-                  if (selectedNotifications.size == notifications?.length) {
-                    setSelectedNotifications(s);
-                  } else {
-                    notifications.forEach((item) => {
-                      s.add(item._id);
-                    });
-                    setSelectedNotifications(s);
-                  }
-                  setKey(Math.random());
-                }}
-              />
-              <span className="ml-1">All</span>
-            </div>
-            <button
-              type="button"
-              onClick={handleMarkRead}
-              className="bg-pink-500 text-white rounded-md px-2 py-[1px]"
-            >
-              Mark As Read
-            </button>
-          </div>
+	return (
+		<>
+			<div className="p-4 w-full h-screen">
+				<h1 className="text-2xl font-medium">Notifications</h1>
+				<div className="h-1 mt-2 mb-4 rounded-full bg-black"></div>
+				<div className="flex flex-col  overflow-y-scroll h-[89vh]" key={key}>
+					<div className="flex flex-row gap-2 justify-start items-center mb-4">
+						<div>
+							<input
+								type={"checkbox"}
+								checked={notifications?.length === selectedNotifications.size}
+								onClick={() => {
+									let s = new Set();
+									if (selectedNotifications.size == notifications?.length) {
+										setSelectedNotifications(s);
+									} else {
+										notifications.forEach((item) => {
+											s.add(item._id);
+										});
+										setSelectedNotifications(s);
+									}
+									setKey(Math.random());
+								}}
+							/>
+							<span className="ml-1">All</span>
+						</div>
+						<button
+							type="button"
+							onClick={handleMarkRead}
+							className="bg-pink-500 text-white rounded-md px-2 py-[1px]"
+						>
+							Mark As Read
+						</button>
+					</div>
 
-          {notifications?.map(({ msg, forUsers, _id }, idx) => {
-            return (
-              <>
-                <NotificationCard
-                  _id={_id}
-                  msg={msg}
-                  read={forUsers[0].read}
-                  createdAt={forUsers[0]?.createdAt}
-                  selectedNotifications={selectedNotifications}
-                  setSelectedNotifications={setSelectedNotifications}
-                  setKey={setKey}
-                />
-              </>
-            );
-          })}
-        </div>
-      </div>
-      <ToastContainer onClick={() => {}} />
-    </>
-  );
+					{notifications?.map(({ msg, forUsers, _id }, idx) => {
+						return (
+							<>
+								<NotificationCard
+									_id={_id}
+									msg={msg}
+									read={forUsers[0].read}
+									createdAt={forUsers[0]?.createdAt}
+									selectedNotifications={selectedNotifications}
+									setSelectedNotifications={setSelectedNotifications}
+									setKey={setKey}
+								/>
+							</>
+						);
+					})}
+				</div>
+			</div>
+			<ToastContainer onClick={() => {}} />
+		</>
+	);
 };
 
 const NotificationCard = ({
-  _id,
-  msg,
-  read,
-  createdAt,
-  selectedNotifications,
-  setSelectedNotifications,
-  setKey,
+	_id,
+	msg,
+	read,
+	createdAt,
+	selectedNotifications,
+	setSelectedNotifications,
+	setKey,
 }) => {
-  const dt = new Date(createdAt);
+	const dt = new Date(createdAt);
 
-  return (
-    <>
-      <div
-        className={`border-b  flex space-x-4  p-2 ${
-          read ? "" : "bg-slate-200"
-        }`}
-      >
-        <input
-          type={"checkbox"}
-          checked={selectedNotifications.has(_id)}
-          onClick={() => {
-            let s = selectedNotifications;
-            if (s.has(_id)) {
-              s.delete(_id);
-            } else {
-              s.add(_id);
-            }
-            // setCheckKey(Math.random());
-            setSelectedNotifications(s);
-            setKey(Math.random());
-          }}
-        />
-        <img src="../images/avatar.jpg" className="w-12 h-12 rounded-full " />
-        <div>
-          {/* <h4 className="font-medium">{read ? "read" : "unread"}</h4> */}
-          <p className="text-gray-600 text-sm">
-            {dt ? `${dt.getHours()}:${dt.getMinutes()}` : ""}
-          </p>
-          <h4>{msg}</h4>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className={`border-b  flex space-x-4  p-2 ${
+					read ? "" : "bg-slate-200"
+				}`}
+			>
+				<input
+					type={"checkbox"}
+					checked={selectedNotifications.has(_id)}
+					onClick={() => {
+						let s = selectedNotifications;
+						if (s.has(_id)) {
+							s.delete(_id);
+						} else {
+							s.add(_id);
+						}
+						// setCheckKey(Math.random());
+						setSelectedNotifications(s);
+						setKey(Math.random());
+					}}
+				/>
+				<img src="../images/avatar.jpg" className="w-12 h-12 rounded-full " />
+				<div>
+					{/* <h4 className="font-medium">{read ? "read" : "unread"}</h4> */}
+					<p className="text-gray-600 text-sm">
+						{dt ? `${dt.getHours()}:${dt.getMinutes()}` : ""}
+					</p>
+					<h4>{msg}</h4>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Notifications;
